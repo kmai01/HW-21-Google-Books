8ae7c7229722eb60cdd0877a679b8393aa049cb4
Khanh Mai <57655821+kmai01@users.noreply.github.com>
2020-05-26 22:17:41 -0500

    add header

---
+++ client/src/Components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const headerStyle = {
+    textAlignments:"center"
+
+}
+function Header() {
+    return (
+        <header style={headerStyle}>
+            <h1>Google Books Search</h1>
+            
+        </header>
+    );
+}
+
+export default Header;
