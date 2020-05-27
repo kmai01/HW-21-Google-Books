c4170c678903597f6a9f03a3b9a89fa1e67a160e
Khanh Mai <57655821+kmai01@users.noreply.github.com>
2020-05-26 20:26:22 -0500

    add component

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -4,7 +4,24 @@ export default {
 // Gets all books
 getBooks: function() {
 
-    
-}
+return axios.get("/api/books");
+
+},
+// Get the book with the given id
+
+getBook: function(id) {
+
+    return axios.get("api/books/" + id)
+},
+
+deleteBook: function(id) {
+
+    return axios.delete("api/books/" + id)
+},
+
+saveBook: function(bookData) {
+
+    return  axios.post("/api/books", bookData)
 
 }
+};
